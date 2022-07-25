a5c8311dca518c2bde1d3a66e6219b036e6fc625
Ryan Walsh <rcwalsh@gmail.com>
2022-07-25 09:14:33 -0400

    add witgen macro for RAEN

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedMap;
 use near_sdk::{env, log, near_bindgen, serde_json, AccountId, Balance, PanicOnDefault, Promise};
+use witgen::witgen;
 
+#[witgen]
 type Amount = Balance;
 type MatcherAccountId = AccountId;
 type MatcherAmountMap = UnorderedMap<MatcherAccountId, Amount>; // https://doc.rust-lang.org/reference/items/type-aliases.html
