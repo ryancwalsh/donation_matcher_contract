a9be3102747586461d2ca1d637913118117bcf27
Ryan Walsh <rcwalsh@gmail.com>
2022-07-27 11:52:47 -0400

    still working on the helper functions

--- src/helpers.rs
+++ src/helpers.rs
@@ -5,6 +5,9 @@ pub mod generic {
 
     pub type FormattedNearString = String; // (commas, underscores, and 'Ⓝ' are acceptable and will be ignored)
 
+    const YOCTO_FACTOR: u128 = u128::pow(10, 24); // https://nomicon.io/Economics/Economic
+    pub const DEFAULT_DECIMAL_PLACES: u8 = 4;
+
     /// Aux functions to interact with the validator
     // https://docs.near.org/develop/contracts/crosscontract#snippet-sending-information
     pub fn did_promise_succeed() -> bool {
@@ -24,36 +27,36 @@ pub mod generic {
         env::sha256_array(account_id.as_bytes())
     }
 
-    /// Helper function to convert yoctoNEAR to $NEAR with 4 decimals of precision.
-    pub(crate) fn yocto_to_near(yocto: u128) -> f64 {
-        //10^20 yoctoNEAR (1 NEAR would be 10_000). This is to give a precision of 4 decimal places.
-        let formatted_near = yocto / 100_000_000_000_000_000_000;
-        let near = formatted_near as f64 / 10_000_f64;
-
+    /// Helper function to convert yoctoNEAR to $NEAR with _ decimals of precision.
+    pub(crate) fn yocto_to_near(amount_in_yocto: u128, decimal_places: u8) -> f64 {
+        // TODO: Audit
+        let mult = u128::pow(10, decimal_places as u32);
+        let formatted_near = amount_in_yocto as f64 / (YOCTO_FACTOR / mult) as f64;
+        let near = formatted_near / mult as f64;
         near
     }
 
-    /// Helper function to convert yoctoNEAR to $NEAR with 4 decimals of precision.
+    /// Helper function to convert yoctoNEAR to $NEAR with _ decimals of precision.
     pub(crate) fn yocto_to_near_string(yocto: u128) -> String {
-        let numeric = yocto_to_near(yocto);
+        let numeric = yocto_to_near(yocto, DEFAULT_DECIMAL_PLACES);
+        // ONEDAY: Add underscores or commas as thousands separators
         numeric.to_string() + &" Ⓝ".to_string()
     }
 
     /// Convert $NEAR to yoctoNEAR.
     pub(crate) fn near_string_to_yocto(near_string: FormattedNearString) -> u128 {
-        // TODO: Audit
         let cleaned = near_string
             .replace(",", "")
             .replace("_", "")
             .replace("Ⓝ", "");
+        // TODO: Audit
         let near: f64 = cleaned.parse().expect("Could not convert NEAR from string to yoctoNEAR integer. Please check the formatting of your string.");
-        let yocto = f64::powi(near, 24); // https://nomicon.io/Economics/Economic
-
+        let yocto = near as u128 * YOCTO_FACTOR;
         near_sdk::log!(
             "near_string_to_yocto converted {} (FormattedNear) to {} (yoctoNEAR)",
             near_string,
             yocto
         );
-        yocto as u128
+        yocto
     }
 }
