b7b9f1c124a6ff0259539902b791f3570a699cc5
Ryan Walsh <rcwalsh@gmail.com>
2022-08-09 12:29:49 -0400

    clean up unit test test_offer_matching_funds_and_get_commitments_and_rescind_matching_funds

--- src/lib_tests.rs
+++ src/lib_tests.rs
@@ -32,20 +32,21 @@ mod lib_tests {
     }
 
     #[test]
-    fn test_offer_matching_funds_and_get_commitments_and_rescind_matching_funds_and_donate() {
+    fn test_offer_matching_funds_and_get_commitments_and_rescind_matching_funds() {
         let mut contract = Contract::new();
+        let starting_balance = near_string_to_yocto(&"1".to_string());
         let recipient = accounts(0); // 0 = Alice
         set_context(
             0, // 0 = Alice
             false,
-            near_string_to_yocto(&"1".to_string()),
+            starting_balance,
             0,
         );
         //log_balance();
         set_context(
             1, // Bob = 1
             false,
-            near_string_to_yocto(&"1".to_string()),
+            starting_balance,
             near_string_to_yocto(&"0.3".to_string()),
         );
         log_balance();
@@ -54,21 +55,21 @@ mod lib_tests {
         set_context(
             2, // Charlie = 2
             false,
-            near_string_to_yocto(&"1".to_string()),
+            starting_balance,
             near_string_to_yocto(&"0.1".to_string()),
         );
         let _matcher2_offer_result = contract.offer_matching_funds(&recipient);
-        // TODO: Assert that this (escrow) contract now contains the correct amount of funds. Assert that the matchers' account balances have decreased appropriately.
+        // Unit tests cannot assert that this (escrow) contract now contains the correct amount of funds. The integration tests should do that and also assert that the matchers' account balances have decreased appropriately.
         log_balance();
         let result = contract.get_commitments(&recipient);
         assert_eq!(
             result,
             "{\"bob\":\"0.3 Ⓝ\",\"charlie\":\"0.1 Ⓝ\"}".to_string()
         );
-        set_context(1, false, near_string_to_yocto(&"1".to_string()), 0);
+        set_context(1, false, starting_balance, 0);
         let _matcher1_rescind_result =
             contract.rescind_matching_funds(&recipient, "0.02 Ⓝ".to_string());
-        // TODO: Assert funds received via transfer. Check state.
+        // Unit tests cannot assert funds received via transfer (check state). The integration tests should.
         let result_after_rescind = contract.get_commitments(&recipient);
         assert_eq!(
             result_after_rescind,
@@ -77,11 +78,8 @@ mod lib_tests {
         set_context(
             3, // Danny = 3
             false,
-            near_string_to_yocto(&"1".to_string()),
+            starting_balance,
             near_string_to_yocto(&"0.1".to_string()),
         );
-        contract.donate(&recipient);
-        // let result_after_donation = contract.get_commitments(&recipient);
-        // assert_eq!(result_after_donation, "{bob: 0.18 Ⓝ,".to_string());
     }
 }
